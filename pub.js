c37470ea075940387316d4ef30d65b5b7f9267f0
Rabble <evan@protest.net>
2019-08-07 09:19:36 -0700

    initial import

---
+++ pub.js
@@ -0,0 +1,22 @@
+// Imports the Google Cloud client library
+const {PubSub} = require('@google-cloud/pubsub');
+
+// Creates a client
+const pubsub = new PubSub();
+
+/**
+ * TODO(developer): Uncomment the following lines to run the sample.
+ */
+const topicName = 'my-topic';
+const data = JSON.stringify({ foo: 'bar' });
+
+// Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
+const dataBuffer = Buffer.from(data);
+
+async function postMessage() {
+    const messageId = await pubsub.topic(topicName).publish(dataBuffer);
+    console.log(`Message ${messageId} published.`);
+
+}
+postMessage();
+
